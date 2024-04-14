179e4f71fa83f348c376e77a784c17fac9212090
Vincent-Preiss <162022555+Vincent-Preiss@users.noreply.github.com>
2024-04-14 09:45:40 +0200

    small bug with "true"->true in DatabaseSettings

--- DataBaseSettingParser/DataBaseSettingParser.js
+++ DataBaseSettingParser/DataBaseSettingParser.js
@@ -1,11 +1,44 @@
 exports.parse = (settings) => {
-    console.log(settings); 
-    return {
-      //databaseSettings
-      name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
+  let returnObj = {}; 
+  for (const [key, value] of Object.entries(settings)) {
+     let innerReturnObj ={}
+    for (const [innerKey, innerValue] of Object.entries(value)) {
+      if (typeof innerValue === "string") {
+        let newInnerValue; 
+        if (innerValue.startsWith('[') && innerValue.endsWith(']')) {
+          newInnerValue = innerValue.replace('[', '');
+          newInnerValue = newInnerValue.replace(']', '');
+          newInnerValue = newInnerValue.split(',');
+          newInnerValue.map(el => {
+            console.log(el);
+            return replaceKeyWordStringsWithKeyWords(el)
+          });
+           innerReturnObj[innerKey] = newInnerValue;
+        }
+        else {
+          innerReturnObj[innerKey] = replaceKeyWordStringsWithKeyWords(
+            innerValue
+          );
+        }
       }
+      else {
+        innerReturnObj[innerKey] = innerValue;
+      }
+    }
+    returnObj[key] = innerReturnObj;
+  }
+    return {
+      returnObj
     };
+}
+
+const replaceKeyWordStringsWithKeyWords = (string) => {
+  switch (string) {
+    case "true":
+      return true; 
+    case "String":
+      return String; 
+    default:
+      return string; 
+  }
 }
